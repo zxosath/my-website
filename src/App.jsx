11e663ffacd9e9bf2cdce2325259fdd43693d232
Sath <Tsathu95@gmail.com>
2025-08-04 00:15:38 +0100

    updated homepage

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          heluuu this is a test from sensenwu
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        helloooo why isnt this working
       </p>
     </>
   )
