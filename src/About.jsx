a99dfc28b68c16aac7b87a1b648644c3507cbb14
Sath <Tsathu95@gmail.com>
2025-08-13 00:09:18 +0100

    paused site with static message

--- src/About.jsx
+++ src/About.jsx
@@ -28,7 +28,7 @@ function About() {
   return (
     <div className="app">
       {/* Header */}
-      <header className={`header ${scrolled ? 'scrolled' : ''}`}>
+      <header className="header scrolled">
         <div className="header-container">
           <div className="logo">
             <a href="#" onClick={(e) => { e.preventDefault(); navigate('/'); }} className="logo-container">
